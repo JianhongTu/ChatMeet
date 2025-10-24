fa39af15b08e091500efa6d0a20af779cb3431fc
JianhongTu <tovitusaikou@gmail.com>
2025-10-23 23:19:43 -0700

    Initial Commit

---
+++ ChatMeet/ChatMeetApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatMeetApp.swift
+//  ChatMeet
+//
+//  Created by jtu22 on 10/23/25.
+//
+
+import SwiftUI
+
+@main
+struct ChatMeetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
